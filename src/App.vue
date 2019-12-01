0ad80bdc2f0f0c6351b3a7b3c8525278d4a95e60
shengjingyin <739178270@qq.com>
2019-12-01 17:57:16 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home1223</router-link>
-      <router-link to="/about">About231231</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
---
+++ src/store/modules/userInfo.js
@@ -0,0 +1,11 @@
+export default {
+  state: {
+    userInfo: {}
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+}
