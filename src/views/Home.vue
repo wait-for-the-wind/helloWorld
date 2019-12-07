2320e9dff02b2da5ee630ba5291840aed59d5458
shengjingyin <739178270@qq.com>
2019-12-07 09:38:22 +0800

    promise。all

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div v-if="indexFind(components, 'andy') >= 0">
+      {{components.indexOf(18)}}
+      66666666666啊老哥
+    </div>
     <router-link to='/about'>关于</router-link>
     <router-view></router-view>
   </div>
@@ -11,8 +15,45 @@
 
 export default {
   name: 'home',
-  components: {
-
+  data () {
+    return {
+      components: [
+        { name: 'andy', age: 18, sexl: 'man' },
+        { name: 'ui', age: 546, sexl: 'trt' },
+        { name: 'hgfh', age: 67, sexl: 'gd' },
+        { name: 'wrewr', age: 8768, sexl: 'ytu' },
+        { name: 'gdfg', age: 5345, sexl: 'gfg' }
+      ]
+    }
+  },
+  mounted () {
+    this.index()
+    this.promise()
+  },
+  methods: {
+    indexFind (str, findV) {
+      return JSON.stringify(str).indexOf(findV)
+    },
+    index () {
+      console.log(JSON.stringify(this.components).indexOf('andy'))
+      console.log('--------------------')
+    },
+    promise () {
+      Promise.all([
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve('resolve1')
+          }, 1000)
+        }),
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve('lve2')
+          }, 2000)
+        })
+      ]).then(res => {
+        console.log(res)
+      })
+    }
   }
 }
 </script>
