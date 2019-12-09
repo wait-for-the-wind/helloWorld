d9e0906118fbfbcd24052398d4bfb6403df5b0f4
老S <739178270@qq.com>
2019-12-09 21:04:18 +0800

    update

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 
 export function request (config) {
+  // 1、创建实例
   const instance1 = axios.create({
     baseURL: 'http://httpbin.org/',
     timeout: 5000
   })
+  // 2、设置拦截器
+  // 3、发送网络请求
   return instance1(config)
 }
 
